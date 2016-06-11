9d0f5e3e7aa462a6bc7c1ebb91701374c6407c5b
Tim Conkling <tconkling@gmail.com>
2016-06-11 13:10:42 -0700

    RAMappedSignal

--- react/RAObjectSignal.h
+++ react/RAObjectSignal.h
@@ -5,6 +5,11 @@
 #import "RAObjectReactor.h"
 
 @interface RAObjectSignal : RAObjectReactor
+
 /** Emits the supplied value to all connected slots. */
 - (void)emitEvent:(id)event;
+
+/** Creates an RAMappedSignal that re-emits mapped versions of this signal's events. */
+- (RAObjectSignal *)map:(id (^)(id value))func;
+
 @end
--- react/RAObjectSignal.m
+++ react/RAObjectSignal.m
@@ -4,9 +4,29 @@
 
 #import "RAObjectSignal.h"
 #import "RAObjectReactor+Protected.h"
+#import "RAMappedSignal.h"
+#import "RAConnection.h"
 
 @implementation RAObjectSignal
+
 - (void)emitEvent:(id)event {
     [self dispatchEvent:event];
 }
+
+- (RAObjectSignal *)map:(id (^)(id value))func {
+    __block RAConnection *conn;
+    __weak typeof(self) weakSelf = self;
+    return [[RAMappedSignal alloc]
+        initWithConnector:^(RAMappedSignal *signal) {
+            typeof(self) strongSelf = weakSelf;
+            conn = [strongSelf connect:^(id o) {
+                [signal emitEvent:func(o)];
+            }];
+
+        } disconnector:^(RAMappedSignal *signal) {
+            [conn disconnect];
+            conn = nil;
+        }];
+}
+
 @end
